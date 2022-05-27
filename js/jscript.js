59da0bb9a142a345d2302ea43a3520a4f781fd5f
TinchoCode <89424821+TinchoCode@users.noreply.github.com>
2022-05-27 18:11:04 -0300

    Add files via upload

--- js/jscript.js
+++ js/jscript.js
@@ -1,3 +1,4 @@
+//FUNCION PARA CAMBIAR ENTRE LAS COSAS DEL NAVBAR
 function tikiTaka(invocoA){
   var v = document.getElementsByClassName('viendo');
   var invocado = document.getElementsByClassName(invocoA);
@@ -12,3 +13,25 @@ function tikiTaka(invocoA){
     v[i].classList.add('escond');
   }
 }
+//////////////////////////////////////////////////////
+//FUNCION PARA CREAR UN DIV CON EL TAG DADO EN EL INPUT 
+//Y VERIFICA SI YA EXISTE, Y MANDA UN ALERT ES ESE CASO
+function bimBam(){
+
+  let valueEsc = document.getElementById("tag").value;
+  var v = document.getElementsByClassName(valueEsc);
+  for(i=0; i<v.length; i++){
+    if(v[i].classList.contains(valueEsc)){
+      window.alert(valueEsc + " ya existe");
+      return;
+    }
+  }
+
+  var element = document.createElement("div");
+  var veamos = document.getElementById("body"); 
+  document.body.insertBefore(element, veamos);
+
+  element.classList.add(valueEsc);
+  element.classList.add('escond');
+}
+//////////////////////////////////////////////////////
